7bb8c95914dc9221d92233bd459680d2699bca92
Daniel Karlsson <danier1999@gmail.com>
2024-12-06 16:04:19 +0100

    cleanup

--- day03/part2.py
+++ day03/part2.py
@@ -4,7 +4,6 @@ file = open("input.txt", "r")
 input = file.read()
 file.close()
 
-input_string = "Here are examples: do() don't() mul(12,34), mul(567,89), don't(), do(), and mul(1234,56)."
 pattern = r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\)|don't\(\))"
 
 matches = re.findall(pattern, input)
