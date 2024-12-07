6bfe0d8b6d37833a8b40eec8904e7d89d4387857
Daniel Karlsson <danier1999@gmail.com>
2024-12-07 08:51:59 +0100

    day07

---
+++ day07/part2.py
@@ -0,0 +1,49 @@
+from typing import Callable, List
+
+
+def add(a: int, b: int):
+    return a + b
+
+
+def mult(a: int, b: int):
+    return a * b
+
+
+def concat(a: int, b: int):
+    return int(str(a) + str(b))
+
+
+def can_match(
+    sum: int,
+    result: int,
+    terms: List[int],
+    possible_operations: List[Callable[[int, int], int]] = [add, mult, concat],
+):
+    if len(terms) == 0:
+        return result == sum
+
+    for operation in possible_operations:
+        next_value = terms[0]
+        local_sum = operation(sum, next_value)
+        if can_match(local_sum, result, terms[1:], possible_operations):
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    file = open("input.txt", "r")
+    input = file.read()
+    file.close()
+
+    lines = input.splitlines()
+    total_sum = 0
+    for line in lines:
+        result, rest = line.split(":")
+        result = int(result)
+        terms = [int(elem) for elem in rest.split(" ")[1:]]
+        # print(terms)
+        if can_match(terms[0], result, terms[1:]):
+            total_sum += result
+
+    print(total_sum)
